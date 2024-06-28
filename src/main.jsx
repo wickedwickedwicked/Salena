c9640a4e9bc743b68b29616714ea70fcb2b6c32b
hamza <hamzachaudhry02@gmail.com>
2024-06-27 20:09:53 -0400

    add: deploy workflows

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/SalenasBlog/">
+    <Route path="/" element={<Home/>}></Route>
     <App />
   </BrowserRouter>,
 )
