aba78a9cbe1e550516e2133d3c2688d1efe70ec4
hamza <hamzachaudhry02@gmail.com>
2024-07-02 17:18:55 -0400

    logos

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import PoemsPage from './Pages/PoemsPage'
 import Contact from './Pages/Contact'
 import ReviewOfAsLong from './Pages/ReviewOfAsLong'
 import NavigatingIstanbul from './Pages/NavigatingIstanbul'
-import { Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import ChoosingFashion from './Pages/ChoosingFashion'
 import WhatWillPeople from './Pages/WhatWillPeople'
 import PleasureCompany from './Pages/PleasureCompany'
@@ -23,24 +23,21 @@ function App() {
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/poems" element={<PoemsPage/>}></Route>
-      <Route path="/contact" element={<Contact/>}></Route>
-      <Route path="/blog/review-of-as-long-as-the-lemon-trees-grow" element={<ReviewOfAsLong/>}></Route>
-      <Route path="/blog/navigating-istanbul" element={<NavigatingIstanbul/>}></Route>
-      <Route path="/blog/choosing-sustainable-fashion" element={<ChoosingFashion/>}></Route>
-      <Route path="/blog/what-will-people-say" element={<WhatWillPeople/>}></Route>
-      <Route path="/blog/pleasure-of-ones-company" element={<PleasureCompany/>}></Route>
-      <Route path="/blog/post-graduation-anxiety" element={<PostGrad/>}></Route>
-      <Route path="/blog/a-hrefhttpssalenachaudhrywordpresscom20210831the-pandemic-has-affected-the-way-we-socializethe-pandemic-has-affected-the-way-we-socializea" element={<PandemicSocialize/>}></Route>
-      <Route path="/poems/composure" element={<ComposurePoem/>}></Route>
-      <Route path="/poems/beauty" element={<BeautyPoem/>}></Route>
-      
-     
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/poems" element={<PoemsPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog/review-of-as-long-as-the-lemon-trees-grow" element={<ReviewOfAsLong />} />
+        <Route path="/blog/navigating-istanbul" element={<NavigatingIstanbul />} />
+        <Route path="/blog/choosing-sustainable-fashion" element={<ChoosingFashion />} />
+        <Route path="/blog/what-will-people-say" element={<WhatWillPeople />} />
+        <Route path="/blog/pleasure-of-ones-company" element={<PleasureCompany />} />
+        <Route path="/blog/post-graduation-anxiety" element={<PostGrad />} />
+        <Route path="/blog/a-hrefhttpssalenachaudhrywordpresscom20210831the-pandemic-has-affected-the-way-we-socializethe-pandemic-has-affected-the-way-we-socializea" element={<PandemicSocialize />} />
+        <Route path="/poems/composure" element={<ComposurePoem />} />
+        <Route path="/poems/beauty" element={<BeautyPoem />} />
+      </Routes>
     </>
   )
 }
