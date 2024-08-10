20a2ac62f70212369712c767ba82157dbb453833
hamza <hamzachaudhry02@gmail.com>
2024-08-10 18:14:59 -0400

    Adding Tracking Test

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -9,8 +9,8 @@ function Contact() {
      ReactGA.pageview(window.location.pathname + window.location.search);
     
      // Optionally, you can also set the page title for better analytics
-     document.title = 'Home';
-     
+     document.title = 'Contact';
+
     window.scrollTo(0, 0); // Scrolls to the top when component mounts
 }, []);
   return (
